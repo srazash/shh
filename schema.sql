1254d3d30c4b4c5c29c119444a7eef613738dca8
Ryan Shaw-Harrison <srazash@protonmail.com>
2025-03-25 16:57:24 +0000

    Database schema defined

---
+++ schema.sql
@@ -0,0 +1,41 @@
+-- application config
+create table config (
+    id          INT PRIMARY KEY AUTOINCREMENT,
+    key         VARCHAR(255) NOT NULL UNIQUE,
+    value       TEXT NOT NULL
+);
+
+-- authentication
+create table users (
+    id          INT PRIMARY KEY AUTOINCREMENT,
+    username    VARCHAR(100) NOT NULL UNIQUE,
+    password    VARCHAR(255) NOT NULL,
+    active      BOOLEAN DEFAULT TRUE
+);
+
+-- secret management
+create table secret (
+    id          INT PRIMARY KEY AUTOINCREMENT,
+    key         VARCHAR(255) NOT NULL UNIQUE,
+    value       TEXT NOT NULL,
+    description TEXT,
+    timestamp   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+create table secret_type (
+    id          INT PRIMARY KEY AUTOINCREMENT,
+    type        VARCHAR(255) NOT NULL,
+    key_name    VARCHAR(255) NOT NULL,
+    value_name  VARCHAR(255) NOT NULL
+);
+
+insert into secret_type (type, key_name, value_name)
+values ('credential', 'Username', 'Password'), ('api_key', 'Key', 'Value');
+
+create table secret_history (
+    id          INT PRIMARY KEY AUTOINCREMENT,
+    secret_id   INT NOT NULL,
+    key         VARCHAR(255) NOT NULL UNIQUE,
+    value       TEXT NOT NULL,
+    timestamp   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
